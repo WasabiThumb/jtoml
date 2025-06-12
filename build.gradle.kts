4ce73f06ef56ac6cf7aab7f9c79683ae6db04fa0
Xavier Pedraza <wasabithumbs@gmail.com>
2025-06-12 15:04:49 -0400

    Fix regression in float serialization (#18)
    * Fix regression in float serialization
    
    * README housekeeping
    
    * chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ allprojects {
     apply(plugin = "net.thebugmc.gradle.sonatype-central-portal-publisher")
 
     group = "io.github.wasabithumb"
-    version = "0.6.0"
+    version = "0.6.1"
 
     dependencies {
         compileOnly("org.jetbrains:annotations:26.0.1")
