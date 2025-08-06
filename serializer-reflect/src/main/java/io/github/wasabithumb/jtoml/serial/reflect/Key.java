6f9722bd3645fdd01c95552477bb5e46f1f5f47f
Xavier Pedraza <xpedraza542@gmail.com>
2025-08-06 12:08:23 -0400

    Propagate version info

--- serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/Key.java
+++ serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/Key.java
@@ -1,5 +1,6 @@
 package io.github.wasabithumb.jtoml.serial.reflect;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.*;
@@ -8,7 +9,9 @@ import java.lang.annotation.*;
  * Annotation for use in {@link io.github.wasabithumb.jtoml.serial.TomlSerializable reflect serialization}.
  * Overrides the TOML key which the annotated field or record component maps to. By default, the key is
  * equal to the name of the field.
+ * @since 1.1.0
  */
+@ApiStatus.AvailableSince("1.1.0")
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD, ElementType.TYPE})
