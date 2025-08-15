a5e10f3870224293cc69c9abc5da3d7ca2a8e1b6
Xavier Pedraza <xpedraza542@gmail.com>
2025-08-15 17:16:35 -0400

    Include service files in aggregator

--- all/build.gradle.kts
+++ all/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.vanniktech.maven.publish.SonatypeHost
+import java.io.*
 
 plugins {
     alias(libs.plugins.publish)
@@ -23,7 +24,6 @@ java {
     }
 
     modularity.inferModulePath = false
-    withJavadocJar()
     withSourcesJar()
 }
 
@@ -60,11 +60,66 @@ tasks.javadoc {
     }
 }
 
+fun blitFile(src: File, target: File) {
+    if (target.exists()) {
+        // Treat as service file
+        val lines = mutableListOf<String>()
+        FileInputStream(src).use { inputStream ->
+            InputStreamReader(inputStream, Charsets.UTF_8).useLines { sequence ->
+                lines.addAll(sequence)
+            }
+        }
+        FileInputStream(target).use { inputStream ->
+            InputStreamReader(inputStream, Charsets.UTF_8).useLines { sequence ->
+                lines.addAll(sequence)
+            }
+        }
+        FileOutputStream(target).use { outputStream ->
+            OutputStreamWriter(outputStream, Charsets.UTF_8).use { writer ->
+                lines.forEach { line ->
+                    writer.write(line)
+                    writer.write('\n'.code)
+                }
+            }
+        }
+    } else {
+        src.copyTo(target, true)
+    }
+}
+
+fun recursiveCopy(src: File, target: File) {
+    if (!src.exists()) return
+    if (!target.exists()) target.mkdirs()
+
+    val files = src.listFiles() ?:
+        throw Error("Failed to list directory $src")
+
+    files.forEach { sub ->
+        val dest = File(target, sub.name)
+        if (sub.isDirectory) {
+            recursiveCopy(sub, File(target, sub.name))
+        } else {
+            blitFile(sub, dest)
+        }
+    }
+}
+
 tasks.processResources {
-    // Shade module classes
+    val tmp = project.layout.buildDirectory.dir("tmp/peerResources")
+
+    // Shade module classes and resources
     peers.forEach { src ->
+        dependsOn(src.tasks.processResources)
         dependsOn(src.tasks.assemble)
         from(src.layout.buildDirectory.dir("classes/java/main"))
+        from(tmp)
+    }
+
+    doFirst {
+        peers.forEach { peers ->
+            val resources = peers.layout.buildDirectory.dir("resources/main")
+            recursiveCopy(resources.get().asFile, tmp.get().asFile)
+        }
     }
 }
 
