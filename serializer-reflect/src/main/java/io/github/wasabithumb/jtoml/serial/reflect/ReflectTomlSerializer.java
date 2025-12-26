f96df6384709a9146a5c3794bafeca7814cb3bd7
Xavier Pedraza <xpedraza542@gmail.com>
2025-12-26 04:07:52 -0500

    Expose reflect serializer API

--- serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/ReflectTomlSerializer.java
+++ serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/ReflectTomlSerializer.java
@@ -1,6 +1,7 @@
 package io.github.wasabithumb.jtoml.serial.reflect;
 
 import io.github.wasabithumb.jtoml.key.TomlKey;
+import io.github.wasabithumb.jtoml.serial.TomlSerializable;
 import io.github.wasabithumb.jtoml.serial.TomlSerializer;
 import io.github.wasabithumb.jtoml.serial.reflect.adapter.TypeAdapter;
 import io.github.wasabithumb.jtoml.serial.reflect.adapter.TypeAdapters;
@@ -12,15 +13,98 @@ import io.github.wasabithumb.jtoml.util.ReferenceHolder;
 import io.github.wasabithumb.jtoml.value.TomlValue;
 import io.github.wasabithumb.jtoml.value.array.TomlArray;
 import io.github.wasabithumb.jtoml.value.table.TomlTable;
+import io.github.wasabithumb.recsup.RecordSupport;
+import org.intellij.lang.annotations.MagicConstant;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
+import java.lang.reflect.Modifier;
+
+/**
+ * Handles reflection-powered conversion of TOML tables
+ * to/from a given table-like Java type. Supports
+ * both records and POJOs marked with {@link TomlSerializable}.
+ * @see #fromToml(TomlTable)
+ * @see #toToml(Object)
+ */
+public final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
+
+    static final int C_SERIALIZE   = 1;
+    static final int C_DESERIALIZE = 2;
+
+    private static void checkType(
+            @NotNull Class<?> type,
+            @MagicConstant(flags = { C_SERIALIZE, C_DESERIALIZE }) int capabilities
+    ) throws IllegalArgumentException {
+        if (RecordSupport.isRecord(type)) return;
+        if (TomlSerializable.class.isAssignableFrom(type)) {
+            if ((capabilities & C_SERIALIZE) == 0) return;
+            int mod = type.getModifiers();
+            if (Modifier.isInterface(mod) || Modifier.isAbstract(mod)) {
+                raiseTypeError(type, capabilities, "not directly instantiable");
+            }
+        } else {
+            raiseTypeError(type, capabilities, "does not implement TomlSerializable and is not a record");
+        }
+    }
+
+    @Contract("_, _, _ -> fail")
+    private static void raiseTypeError(
+            @NotNull Class<?> type,
+            @MagicConstant(flags = { C_SERIALIZE, C_DESERIALIZE }) int capabilities,
+            @NotNull String detail
+    ) {
+        String classifier = ((capabilities & C_SERIALIZE) == 0) ? "deserializer" : "serializer";
+        throw new IllegalArgumentException("Cannot create " + classifier + " for " + type.getName() +
+                " (" + detail + ")");
+    }
+
+    //
 
     private final TableTypeModel<T> model;
+    private final TypeAdapters adapters;
+    private final int capabilities;
 
-    ReflectTomlSerializer(@NotNull Class<T> clazz) {
-        this.model = TypeModel.of(new ParameterizedClass<>(clazz))
-                .asTable();
+    /**
+     * Create a new serializer for converting objects of the given
+     * type to/from a TOML table. The newly created serializer will
+     * use the {@link TypeAdapters#standard() standard set} of adapters.
+     * @param type The table-like type to convert to/from.
+     * @throws IllegalArgumentException The given type is not serializable.
+     * @see #ReflectTomlSerializer(Class, TypeAdapters)
+     */
+    public ReflectTomlSerializer(
+            @NotNull Class<T> type
+    ) throws IllegalArgumentException {
+        this(type, TypeAdapters.standard());
+    }
+
+    /**
+     * Create a new serializer for converting objects of the given
+     * type to/from a TOML table.
+     * @param type The table-like type to convert to/from.
+     * @param adapters Describes how leaves in the document tree may be converted to/from Java objects.
+     * @throws IllegalArgumentException The given type is not serializable.
+     * @see #ReflectTomlSerializer(Class)
+     */
+    public ReflectTomlSerializer(
+            @NotNull Class<T> type,
+            @NotNull TypeAdapters adapters
+    ) throws IllegalArgumentException {
+        this(type, adapters, C_SERIALIZE | C_DESERIALIZE);
+    }
+
+    ReflectTomlSerializer(
+            @NotNull Class<T> type,
+            @NotNull TypeAdapters adapters,
+            @MagicConstant(flags = { C_SERIALIZE, C_DESERIALIZE }) int capabilities
+    ) {
+        checkType(type, capabilities);
+        TableTypeModel<T> model = TableTypeModel.match(new ParameterizedClass<>(type));
+        assert model != null;
+        this.model = model;
+        this.adapters = adapters;
+        this.capabilities = capabilities;
     }
 
     //
@@ -32,6 +116,7 @@ final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
 
     @Override
     public @NotNull T fromToml(@NotNull TomlTable table) {
+        if ((this.capabilities & C_SERIALIZE) == 0) throw new UnsupportedOperationException();
         return this.serializeTable(
                 this.model,
                 table
@@ -40,6 +125,7 @@ final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
 
     @Override
     public @NotNull TomlTable toToml(@NotNull T data) {
+        if ((this.capabilities & C_DESERIALIZE) == 0) throw new UnsupportedOperationException();
         ReferenceHolder parents = new ReferenceHolder();
         parents.add(this);
         return this.deserializeTable(
@@ -68,7 +154,7 @@ final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
         if (model.isArray()) return this.serializeArray(model.asArray(), value.asArray());
         if (model.isTable()) return this.serializeTable(model.asTable(), value.asTable());
 
-        TypeAdapter<E> adapter = TypeAdapters.get(model.type());
+        TypeAdapter<E> adapter = this.adapters.getAssert(model.type());
         return adapter.toJava(value.asPrimitive());
     }
 
@@ -129,7 +215,7 @@ final class ReflectTomlSerializer<T> implements TomlSerializer.Symmetric<T> {
         if (model.isArray()) return this.deserializeArray(parents, model.asArray(), value);
         if (model.isTable()) return this.deserializeTable(parents, model.asTable(), value);
 
-        TypeAdapter<E> adapter = TypeAdapters.get(model.type());
+        TypeAdapter<E> adapter = this.adapters.getAssert(model.type());
         return adapter.toToml(value);
     }
 
--- serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/adapter/TypeAdapters.java
+++ serializer-reflect/src/main/java/io/github/wasabithumb/jtoml/serial/reflect/adapter/TypeAdapters.java
@@ -1,159 +1,111 @@
 package io.github.wasabithumb.jtoml.serial.reflect.adapter;
 
-import io.github.wasabithumb.jtoml.value.primitive.TomlPrimitive;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collection;
 
-import static io.github.wasabithumb.jtoml.serial.reflect.adapter.TypeAdapter.*;
+/**
+ * An immutable collection of {@link TypeAdapter}s for use
+ * in {@link io.github.wasabithumb.jtoml.serial.reflect.ReflectTomlSerializer reflect serialization}.
+ * @see #builder()
+ * @see #standard()
+ */
+@ApiStatus.NonExtendable
+public interface TypeAdapters extends Collection<TypeAdapter<?>> {
 
-@ApiStatus.Internal
-public final class TypeAdapters {
-
-    private static final Map<Class<?>, TypeAdapter<?>> MAP;
-    static {
-        Map<Class<?>, TypeAdapter<?>> map = new HashMap<>();
-        TypeAdapter<?> tmp;
-
-        // Boolean
-        tmp = create(
-                Boolean.class,
-                TomlPrimitive::asBoolean,
-                TomlPrimitive::of
-        );
-        map.put(Boolean.class, tmp);
-        map.put(Boolean.TYPE, tmp);
-
-        // Byte
-        tmp = create(
-                Byte.class,
-                (TomlPrimitive v) -> (byte) v.asInteger(),
-                (Byte b) -> TomlPrimitive.of(b.intValue())
-        );
-        map.put(Byte.class, tmp);
-        map.put(Byte.TYPE, tmp);
-
-        // Short
-        tmp = create(
-                Short.class,
-                (TomlPrimitive v) -> (short) v.asInteger(),
-                (Short s) -> TomlPrimitive.of(s.intValue())
-        );
-        map.put(Short.class, tmp);
-        map.put(Short.TYPE, tmp);
-
-        // Integer
-        tmp = create(
-                Integer.class,
-                TomlPrimitive::asInteger,
-                TomlPrimitive::of
-        );
-        map.put(Integer.class, tmp);
-        map.put(Integer.TYPE, tmp);
-
-        // Long
-        tmp = create(
-                Long.class,
-                TomlPrimitive::asLong,
-                TomlPrimitive::of
-        );
-        map.put(Long.class, tmp);
-        map.put(Long.TYPE, tmp);
-
-        // Float
-        tmp = create(
-                Float.class,
-                TomlPrimitive::asFloat,
-                TomlPrimitive::of
-        );
-        map.put(Float.class, tmp);
-        map.put(Float.TYPE, tmp);
-
-        // Double
-        tmp = create(
-                Double.class,
-                TomlPrimitive::asDouble,
-                TomlPrimitive::of
-        );
-        map.put(Double.class, tmp);
-        map.put(Double.TYPE, tmp);
-
-        // Character
-        tmp = create(
-                Character.class,
-                (TomlPrimitive tp) -> {
-                    if (tp.isInteger()) {
-                        return (char) tp.asInteger();
-                    } else {
-                        String s = tp.asString();
-                        if (s.length() != 1) throw new IllegalStateException("Cannot convert multi-char string to char");
-                        return s.charAt(0);
-                    }
-                },
-                (Character c) -> TomlPrimitive.of(Character.toString(c))
-        );
-        map.put(Character.class, tmp);
-        map.put(Character.TYPE, tmp);
-
-        // String
-        tmp = create(
-                String.class,
-                TomlPrimitive::asString,
-                TomlPrimitive::of
-        );
-        map.put(String.class, tmp);
-
-        // Offset Date-Time
-        tmp = create(
-                OffsetDateTime.class,
-                TomlPrimitive::asOffsetDateTime,
-                TomlPrimitive::of
-        );
-        map.put(OffsetDateTime.class, tmp);
-
-        // Local Date-Time
-        tmp = create(
-                LocalDateTime.class,
-                TomlPrimitive::asLocalDateTime,
-                TomlPrimitive::of
-        );
-        map.put(LocalDateTime.class, tmp);
-
-        // Local Date
-        tmp = create(
-                LocalDate.class,
-                TomlPrimitive::asLocalDate,
-                TomlPrimitive::of
-        );
-        map.put(LocalDate.class, tmp);
-
-        // Local Time
-        tmp = create(
-                LocalTime.class,
-                TomlPrimitive::asLocalTime,
-                TomlPrimitive::of
-        );
-        map.put(LocalTime.class, tmp);
-
-        MAP = map;
+    /**
+     * Provides a new builder for creating
+     * custom {@link TypeAdapter} collections.
+     * This builder is initially empty and does not
+     * contain any of the
+     * {@link #standard() standard adapters}.
+     */
+    @Contract("-> new")
+    static @NotNull Builder builder() {
+        return new TypeAdaptersImpl.Builder();
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> @NotNull TypeAdapter<T> get(@NotNull Class<T> type) {
-        TypeAdapter<?> ret = MAP.get(type);
-        if (ret == null)
-            throw new IllegalArgumentException("No registered adapter for type "  + type.getName());
-        return (TypeAdapter<T>) ret;
+    /**
+     * Provides a {@link TypeAdapters} instance
+     * containing every constant defined
+     * in {@link TypeAdapter}. This is the default
+     * set of adapters used by the reflect serializer.
+     */
+    @Contract(pure = true)
+    static @NotNull TypeAdapters standard() {
+        return TypeAdaptersImpl.STANDARD;
     }
 
     //
 
-    private TypeAdapters() { }
+    @ApiStatus.Internal
+    <T> @Nullable TypeAdapter<T> get(@NotNull Class<T> type);
+
+    @ApiStatus.Internal
+    default <T> @NotNull TypeAdapter<T> getAssert(@NotNull Class<T> type) throws IllegalArgumentException {
+        TypeAdapter<T> adapter = this.get(type);
+        if (adapter == null) throw new IllegalArgumentException("No adapter for type " + type.getName());
+        return adapter;
+    }
+
+    //
+
+    /**
+     * Handles creation of custom {@link TypeAdapters}
+     * collections.
+     * @see #clear()
+     * @see #add(TypeAdapter)
+     * @see #add(Collection)
+     * @see #build()
+     */
+    @ApiStatus.NonExtendable
+    interface Builder {
+
+        /**
+         * Clears the builder,
+         * removing all adapters previously
+         * added to it.
+         */
+        @Contract("-> this")
+        @NotNull Builder clear();
+
+        /**
+         * Adds a new type adapter.
+         * If an adapter with the same {@link TypeAdapter#typeClass() type class}
+         * was already added, that adapter is replaced.
+         * If the adapter's type class is
+         * primitive, it is additionally added as if it were
+         * the boxed variant.
+         */
+        @Contract("_ -> this")
+        @NotNull Builder add(@NotNull TypeAdapter<?> adapter);
+
+        /**
+         * Adds each type adapter contained in the
+         * given collection to this builder.
+         * Can be used to inherit the standard adapters
+         * via {@code add(TypeAdapters.standard())}.
+         * @see #add(TypeAdapter)
+         */
+        @Contract("_ -> this")
+        default @NotNull Builder add(@NotNull Collection<TypeAdapter<?>> adapters) {
+            for (TypeAdapter<?> adapter : adapters) {
+                this.add(adapter);
+            }
+            return this;
+        }
+
+        /**
+         * Provides a new {@link TypeAdapters} instance
+         * containing all adapters added to this builder.
+         * The builder may not be used after this point.
+         */
+        @Contract("-> new")
+        @NotNull TypeAdapters build();
+
+    }
 
 }
