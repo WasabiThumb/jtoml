b0758963fbc787f88ca9dd8d83d6d8d116ad27e4
Xavier Pedraza <xpedraza542@gmail.com>
2026-09-25 00:35:52 -0400

    Fix signalling NULL leaking into documents when indentation policy is NONE (#88)
    * Fix signalling NULL leaking into documents when indentation policy is NONE
    
    * Add elementIndent field to IndentationPolicy

--- api/src/main/java/io/github/wasabithumb/jtoml/option/prop/IndentationPolicy.java
+++ api/src/main/java/io/github/wasabithumb/jtoml/option/prop/IndentationPolicy.java
@@ -21,6 +21,9 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
+import java.lang.annotation.*;
+import java.util.Arrays;
+
 /**
  * Determines how a TOML document should be indented
  * when writing.
@@ -45,60 +48,86 @@ import org.jetbrains.annotations.Range;
  *         The indentation level is incremented by {@link #postIndent()} directly after each table
  *         declaration.
  *     </li>
+ *     <li>
+ *         The indentation level is incremented/decremented by {@link #elementIndent()} before/after
+ *         each value in an array.
+ *     </li>
  * </ul>
  */
 public final class IndentationPolicy {
 
     /** No indentation */
-    public static final IndentationPolicy NONE = new IndentationPolicy('\0', 0x00000000);
+    public static final IndentationPolicy NONE;
 
     /**
-     * indentationLevel is initialized to 0.
-     * Table headers set indentationLevel to {@code key.size() - 1}.
-     * Each line is preceded by "indentationLevel" TAB characters.
+     * A basic sensible indentation policy
+     * with {@code '\t'} (TAB) as the indent character,
+     * a {@link #variableIndent() variable indent} of {@code 1}
+     * and an {@link #elementIndent() element indent} of {@code 1}.
      */
-    public static final IndentationPolicy STANDARD = new IndentationPolicy('\t', 0x00000100);
+    public static final IndentationPolicy STANDARD;
 
     @Contract("-> new")
     public static @NotNull Builder builder() {
         return new Builder();
     }
 
-    //
-
-    private final char indentChar;
-    private final int data;
-
-    private IndentationPolicy(char indentChar, int data) {
-        this.indentChar = indentChar;
-        this.data = data;
+    static {
+        byte[] i0 = new byte[Kind.MAX];
+        byte[] i1 = new byte[Kind.MAX];
+        i1[Kind.VARIABLE] = 1;
+        i1[Kind.ELEMENT] = 1;
+        NONE = new IndentationPolicy('\0', i0);
+        STANDARD = new IndentationPolicy('\t', i1);
     }
 
     //
+
+    private final char indentChar;
+    private final byte[] indentation;
+
+    private IndentationPolicy(char indentChar, byte[] indentation) {
+        this.indentChar = indentChar;
+        this.indentation = indentation;
+    }
+
+    //
+
+    private @Range(from = 0, to = 255) int indentation(@Kind int kind) {
+        return Byte.toUnsignedInt(this.indentation[kind]);
+    }
 
     public char indentChar() {
         return this.indentChar;
     }
 
     public @Range(from=0, to=255) int globalIndent() {
-        return (this.data >> 24) & 0xFF;
+        return this.indentation(Kind.GLOBAL);
     }
 
     public @Range(from=0, to=255) int constantIndent() {
-        return (this.data >> 16) & 0xFF;
+        return this.indentation(Kind.CONSTANT);
     }
 
     public @Range(from=0, to=255) int variableIndent() {
-        return (this.data >> 8) & 0xFF;
+        return this.indentation(Kind.VARIABLE);
     }
 
     public @Range(from=0, to=255) int postIndent() {
-        return this.data & 0xFF;
+        return this.indentation(Kind.POST);
+    }
+
+    public @Range(from=0, to=255) int elementIndent() {
+        return this.indentation(Kind.ELEMENT);
     }
 
     @Override
     public int hashCode() {
-        return Integer.hashCode(this.data);
+        int h = 7;
+        h = 31 * h + (int) this.indentChar;
+        for (int i = 0; i < Kind.MAX; i++)
+            h = 31 * h + (int) this.indentation[i];
+        return h;
     }
 
     @Override
@@ -106,7 +135,7 @@ public final class IndentationPolicy {
         if (!(obj instanceof IndentationPolicy)) return false;
         IndentationPolicy other = (IndentationPolicy) obj;
         if (this.indentChar != other.indentChar) return false;
-        return this.data == other.data;
+        return Arrays.equals(this.indentation, other.indentation);
     }
 
     @Override
@@ -123,6 +152,8 @@ public final class IndentationPolicy {
                 this.variableIndent() +
                 ", postIndent=" +
                 this.postIndent() +
+                ", elementIndent=" +
+                this.elementIndent() +
                 "]";
     }
 
@@ -130,8 +161,8 @@ public final class IndentationPolicy {
 
     public static final class Builder {
 
-        private final int[] values = new int[] { 0, 0, 0, 0 };
         private char indentChar    = '\t';
+        private final byte[] indentation = new byte[Kind.MAX];
 
         //
 
@@ -147,31 +178,36 @@ public final class IndentationPolicy {
         }
 
         @Contract("_, _ -> this")
-        private @NotNull Builder setValue(int index, int value) {
+        private @NotNull Builder indentation(@Kind int kind, int value) {
             if (value < 0) throw new IllegalArgumentException("Indentation level may not be negative");
             if (value > 255) throw new IllegalArgumentException("Indentation level is too large (" + value + " > 255)");
-            this.values[index] = value;
+            this.indentation[kind] = (byte) value;
             return this;
         }
 
         @Contract("_ -> this")
         public @NotNull Builder globalIndent(@Range(from=0, to=255) int indent) {
-            return this.setValue(0, indent);
+            return this.indentation(Kind.GLOBAL, indent);
         }
 
         @Contract("_ -> this")
         public @NotNull Builder constantIndent(@Range(from=0, to=255) int indent) {
-            return this.setValue(1, indent);
+            return this.indentation(Kind.CONSTANT, indent);
         }
 
         @Contract("_ -> this")
         public @NotNull Builder variableIndent(@Range(from=0, to=255) int indent) {
-            return this.setValue(2, indent);
+            return this.indentation(Kind.VARIABLE, indent);
         }
 
         @Contract("_ -> this")
         public @NotNull Builder postIndent(@Range(from=0, to=255) int indent) {
-            return this.setValue(3, indent);
+            return this.indentation(Kind.POST, indent);
+        }
+
+        @Contract("_ -> this")
+        public @NotNull Builder elementIndent(@Range(from=0, to=255) int indent) {
+            return this.indentation(Kind.ELEMENT, indent);
         }
 
         @Contract("_ -> this")
@@ -180,8 +216,8 @@ public final class IndentationPolicy {
 
             int cur;
             int product;
-            for (int i=0; i < 4; i++) {
-                cur = this.values[i];
+            for (int i = 0; i < Kind.MAX; i++) {
+                cur = Byte.toUnsignedInt(this.indentation[i]);
                 try {
                     product = Math.multiplyExact(cur, amount);
                     if (product > 255) product = -1;
@@ -191,7 +227,7 @@ public final class IndentationPolicy {
                 if (product == -1) {
                     throw new IllegalArgumentException("Scale is too large (" + cur + " * " + amount + " > 255)");
                 }
-                this.values[i] = product;
+                this.indentation[i] = (byte) product;
             }
 
             return this;
@@ -203,11 +239,27 @@ public final class IndentationPolicy {
         public @NotNull IndentationPolicy build() {
             return new IndentationPolicy(
                     this.indentChar,
-                    (this.values[0] << 24) | (this.values[1] << 16) |
-                            (this.values[2] << 8) | (this.values[3])
+                    Arrays.copyOf(this.indentation, Kind.MAX)
             );
         }
 
+    }
+
+    /**
+     * Indices in the {@link IndentationPolicy}'s
+     * internal array for each kind of indentation
+     */
+    @Documented
+    @Retention(RetentionPolicy.SOURCE)
+    @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+    @MagicConstant(valuesFromClass = Kind.class)
+    private @interface Kind {
+        int GLOBAL = 0;
+        int CONSTANT = 1;
+        int VARIABLE = 2;
+        int POST = 3;
+        int ELEMENT = 4;
+        int MAX = 5;
     }
 
 }
--- internals/src/main/java/io/github/wasabithumb/jtoml/io/TableWriter.java
+++ internals/src/main/java/io/github/wasabithumb/jtoml/io/TableWriter.java
@@ -74,7 +74,8 @@ public final class TableWriter implements Closeable {
 
     private void writeIndent() {
         final char c = this.options.get(JTomlOption.INDENTATION).indentChar();
-        for (int i=0; i < this.indentLevel; i++) this.out.put(c);
+        if (c == '\0') return;
+        for (int i = 0; i < this.indentLevel; i++) this.out.put(c);
     }
 
     private void writeTableHeader0(
@@ -352,7 +353,7 @@ public final class TableWriter implements Closeable {
 
         if (doNewlines) {
             this.out.put(newline);
-            this.indentLevel++;
+            this.indentLevel += this.options.get(JTomlOption.INDENTATION).elementIndent();
         } else {
             for (int i=0; i < padding.arrayPadding(); i++)
                 this.out.put(' ');
@@ -409,7 +410,7 @@ public final class TableWriter implements Closeable {
         }
 
         if (doNewlines) {
-            this.indentLevel--;
+            this.indentLevel -= this.options.get(JTomlOption.INDENTATION).elementIndent();
             this.writeIndent();
         } else {
             for (int i=0; i < padding.arrayPadding(); i++)
